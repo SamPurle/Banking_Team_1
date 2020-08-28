2a76dc8397c85075a0f4c94f132091b9ef4f713c
Sam Purle <sdpurle@oakseynet.com>
2020-08-28 17:01:03 +0100

    Added Salary Multiple and CrimeRate ranking Metrics

--- Metrics.sql
+++ Metrics.sql
@@ -7,9 +7,6 @@ Salary Metric
 
 select  
 	a.AccountId,
-	CleanDate,
-	SumDep,
-	MaxDate,
 	convert(money,SumDep / (datediff(year, CleanDate, MaxDate))) as Salary
 from Clean.Account as a
 inner join
@@ -29,9 +26,6 @@ Expenses Metric
 
 select  
 	a.AccountId,
-	CleanDate,
-	SumWith,
-	MaxDate,
 	convert(money, SumWith / (datediff(year, CleanDate, MaxDate))) as Expenses
 from Clean.Account as a
 inner join
@@ -51,9 +45,6 @@ Cashflow Metric
 
 select  
 	a.AccountId,
-	CleanDate,
-	SumTrans,
-	MaxDate,
 	convert(money, SumTrans / (datediff(year, CleanDate, MaxDate))) as CashFlow
 from Clean.Account as a
 inner join
@@ -65,3 +56,41 @@ select distinct top 500
 from Clean.Trans	
 ) as t
 on a.AccountId = t.AccountId
+
+/*
+Income compared to District
+*/
+
+select  
+	a.AccountId,	
+	convert(money,SumDep / (datediff(year, CleanDate, MaxDate))) / MeanSalary as SalaryMultiple	
+from Clean.Account as a
+inner join
+(
+select distinct top 500
+	AccountId,
+	sum(amount) over (partition by AccountId) as SumDep,
+	max(CleanDate) over (partition by AccountId) as MaxDate
+from Clean.Trans
+	where amount > 0
+) as t
+on a.AccountId = t.AccountId
+inner join Clean.District as d
+	on a.DistrictId = d.DistrictId
+
+/*
+Rank districts by Crime Rate
+*/
+
+;with CTE_CrimeRate 
+as
+(
+select
+	DistrictId,
+	convert(float, Crimes96) / convert(float, Inhabitants) * 100 as CrimeRate
+from Clean.District
+)
+select
+	*,
+	rank() over (order by CrimeRate desc) as CrimeRank
+from CTE_CrimeRate
